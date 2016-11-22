3d86a246838c201b5e6669b5c508ea9f2b738db4
Mikael Göransson <github@mgor.se>
2016-11-22 17:43:02 +0100

    tmux cleanup, 'plugin'-based

---
+++ .tmux/keybindings.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# set -g prefix §
+map_control_key() {
+    tmux set -g prefix C-s
+    tmux unbind C-b
+    tmux bind C-s send-prefix
+}
+
+# Force a reload of the config file
+map_reload() {
+    tmux unbind r
+    tmux bind r source-file ~/.tmux.conf \; display "Reloaded ~/.tmux.conf"
+}
+
+map_pane_navigation() {
+    # Quick pane cycling
+    tmux unbind ^A
+    tmux bind ^A select-pane -t :.+
+
+    # More straight forward key bindings for splitting
+    tmux unbind %
+    tmux bind | split-window -h
+    tmux bind h split-window -h
+    tmux unbind '"'
+    tmux bind - split-window -v
+    tmux bind v split-window -v
+
+    # Moving between panes
+    tmux bind h select-pane -L
+    tmux bind j select-pane -D
+    tmux bind k select-pane -U
+    tmux bind l select-pane -R
+
+    # Pane resizing
+    tmux bind-key H resize-pane -U 15
+    tmux bind-key J resize-pane -D 15
+    tmux bind-key K resize-pane -L 15
+    tmux bind-key L resize-pane -R 15
+}
+
+map_window_management() {
+    # Better name management
+    tmux bind c new-window
+    tmux bind , command-prompt "rename-window '%%'"
+}
+
+map_copy_paste() {
+    # Vi-style copy paste
+    tmux unbind [
+    tmux bind Escape copy-mode
+    tmux unbind p
+    tmux bind p paste-buffer
+    tmux bind-key -t vi-copy 'v' begin-selection
+    tmux bind-key -t vi-copy 'y' copy-selection
+}
+
+map_clear() {
+    # CLEAR
+    tmux bind -n C-k send-keys "clear; tmux clear-history" \; send-keys "Enter"
+}
+
+for function in $(declare -F | awk '{print $NF}'); do
+    eval "${function}"
+done
---
+++ .tmux/theme.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set_theme_parameters() {
+    tmux set -g status-fg white
+    tmux set -g status-bg colour234
+    tmux set -g window-status-activity-attr bold
+    tmux set -g pane-border-fg colour245
+    tmux set -g pane-active-border-fg colour14
+    tmux set -g message-fg colour16
+    tmux set -g message-bg colour12
+    tmux set -g message-attr bold
+    tmux set -g status-left-length 32
+    tmux set -g status-right-length 150
+    tmux set -g status-interval 5
+    tmux set -g default-terminal "screen-256color"
+}
+
+set_status_left() {
+    # Powerline symbols:        
+    local tmux_version="$(tmux -V | sed -r 's|[^0-9\.]||g')"
+
+    if [[ "$(echo "${tmux_version} >= 2.0" | bc)" -eq 1 ]]; then
+        tmux set -g status-left '#[fg=colour15,bg=colour166,bold] u #[fg=white,bg=colour234] '
+    else
+        tmux set -g status-left '#[fg=colour15,bg=colour166,bold] u '
+    fi
+}
+
+set_status_right() {
+    tmux set -g status-right '#[fg=colour237,bg=colour247] #(date +"%a") %d %b %R #[fg=colour247,bg=colour237] #h '
+}
+
+set_status_window() {
+    tmux set -g window-status-separator ' '
+    tmux set -g window-status-format "#[fg=colour235,bg=colour239,noreverse,nobold] #I 〉#W "
+    tmux set -g window-status-current-format "#[fg=colour234,bg=colour172,noreverse,bold] #I 〉#W "
+}
+
+for function in $(declare -F | awk '{print $NF}'); do
+    eval "${function}"
+done
