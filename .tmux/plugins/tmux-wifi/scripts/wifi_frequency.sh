5e14ad6a48f23bf825e6e3b0cd4c3885fc659041
Mikael Göransson <github@mgor.se>
2017-03-06 13:37:38 +0100

    tmux-wifi: wifi_frequency
    erm, correct behavior this time.

--- .tmux/plugins/tmux-wifi/scripts/wifi_frequency.sh
+++ .tmux/plugins/tmux-wifi/scripts/wifi_frequency.sh
@@ -8,8 +8,7 @@ CURRENT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 main() {
 	local frequency
     frequency="$(iwconfig "$(get_wifi_interface)" | awk '/Frequency/ {gsub(/[^0-9\.]/, "", $2); print $2}')"
-    [[ -z "${frequency}" ]] && frequency="n/a"
-    [[ -n "${frequency}" ]] && frequency="${frequency:0:3} Ghz"
+    [[ -n "${frequency}" ]] && frequency="${frequency:0:3} Ghz" || frequency="n/a"
 
     echo "${frequency}"
 }
